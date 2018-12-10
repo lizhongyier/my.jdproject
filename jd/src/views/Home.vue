fe71700cc73a645dc624905322d1942d1dbb6aaf
lizhongyier <13294814755@163.com>
2018-12-10 11:38:28 +0800

    axios输出内容

--- jd/src/views/Home.vue
+++ jd/src/views/Home.vue
@@ -3,7 +3,9 @@
 		<search-bar></search-bar>
 		<swiper></swiper>
 		<div class="" style="height: 1000px;">
-			
+			<ul>
+				<li v-for="(item,index) in product" v-text="item.title"></li>
+			</ul>
 		</div>
 <footerbar></footerbar>		
 	</div>
@@ -16,6 +18,13 @@
 	import Swiper from "@/components/Swiper.vue";
 	import axios from "axios";
 	export default{
+		data(){
+			return{
+				product:[]
+			}
+		},
+		
+		
 		components:{
 			Footerbar,
 			SearchBar,
@@ -25,6 +34,7 @@
 		mounted(){
 			axios.get("/data.json").then(res=>{
 				console.log(res);
+				this.product=res.data;
 			}).catch(err=>{});
 		}
 	}
